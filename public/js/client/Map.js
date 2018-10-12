838bd73be9e39135df25c67ea702ac83dbaeae8c
Simon Marcoux <sim_marc@hotmail.com>
2018-10-12 17:47:32 -0400

    Begin svg zooming module

--- public/js/client/Map.js
+++ public/js/client/Map.js
@@ -5,8 +5,17 @@ export default class Map extends AbstractDispatcher {
     constructor() {
         super();
 
-        this.map = document.querySelectorAll('.map-wrp svg');
+        this.map = document.querySelector('.map-wrp svg');
+        this.defaultViewBox = this.map.getAttribute('viewBox');
+        this.activeViewBox = this.defaultViewBox;
+        this.tl = new TimelineMax();
+        this.zoomConfig = {
+            in: 0.9, 
+            out: 1.1
+        }
+
         this.handlePathClick(this.getPaths());
+        this.handleZoom();
     }
     
     getPaths() {
@@ -29,4 +38,45 @@ export default class Map extends AbstractDispatcher {
         }
 
     }
+
+    handleZoom() {
+        // inspiration -> https://codepen.io/PointC/pen/XbqBmM
+        const zoomInBtn = document.querySelector('[data-ui="zoom-in"]');
+        const zoomOutBtn = document.querySelector('[data-ui="zoom-out"]');
+        
+
+        zoomInBtn.addEventListener('click', () => {
+            this.zoom('zoomin');
+        });
+
+        zoomOutBtn.addEventListener('click', () => {
+            this.zoom('zoomout');
+        });
+    }
+
+    calculateZoom(direction) {
+        let valuesArr = this.defaultViewBox.split(" ");
+        let newValues = [];
+        let zoomValue = direction === 'zoomin' ? this.zoomConfig.in : this.zoomConfig.out;
+
+        valuesArr.forEach(val => {
+            let viewBoxValue = val * zoomValue;
+            newValues.push(viewBoxValue);
+        });
+
+        return newValues;
+    }
+    
+    
+    
+    zoom(direction) {
+        let values = this.calculateZoom(direction);
+        this.activeViewBox = values.join(" ");
+        this.animateZoom();
+    }
+    
+    animateZoom() {
+        this.tl.to(this.map, 1.5, { attr: { viewBox: this.activeViewBox } });
+    }
+    
 }
